c21d782a1c24262db2154c2b8caccb8b44dc07b0
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-05-18 18:16:26 +0530

    Fixed the icon of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import Logo from "../assets/logo.webp";
 
-const Footer = () => {
-  return (
-    <footer className="bg-[#0c0c0d] border-t border-[#2e2e2e] py-6">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center space-x-2">
-          <img src={Logo} alt="Logo" className="w-5 h-5" />
-          <p className="text-sm text-gray-400">Copyright © 2025</p>
-        </div>
-      </div>
-    </footer>
-  );
+export default function Footer() {
+    return (
+        <footer className="border-t border-[#2e2e2e] px-12">
+            <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+                <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+                    <img
+                        src="https://www.centralaxis.com/logo_white_icon_only.png"
+                        alt="Logo"
+                        width={36}
+                        height={36}
+                        loading="lazy"
+                        style={{ color: "transparent" }}
+                    />
+                    <p className="text-center text-sm leading-loose md:text-left">
+                        Copyright © 2025
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 };
-
-export default Footer;
